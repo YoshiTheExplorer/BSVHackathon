eaaddc47a1b8c9627dac4e023bc4e27fc9f6a4e2
Siddhu Mohan <61168189+Ranger-26@users.noreply.github.com>
2025-04-05 20:40:14 -0500

    get button event handler working

---
+++ src/walletAuth.ts
@@ -0,0 +1,6 @@
+const connectBtn = document.getElementById("connect-btn")!;
+const walletDisplay = document.getElementById("wallet-address")!;
+
+connectBtn.addEventListener("click", () => {
+  console.log("yes")
+});
