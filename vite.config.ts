6442062067f166e28bcba5dcaaa5f5b926f44611
Colin Yamada <colin.yamada@gmail.com>
2025-04-06 05:51:53 -0500

    lol

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,14 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-
 export default defineConfig({
     build: {
         outDir: 'dist',
         rollupOptions: {
             input: {
-                content: resolve(__dirname, 'src/injectYoutube.ts'),
+                injectYoutube: resolve(__dirname, 'src/injectYoutube.ts')
             },
             output: {
-                format: 'iife', // 👈 IMPORTANT: bundle as classic script
-                entryFileNames: '[name].js',
+                format: 'iife', // <- 👈 bundle all imports into one browser-friendly function
+                entryFileNames: '[name].js'
             }
         }
-    },
-    plugins: [
-        viteStaticCopy({
-            targets: [
-                { src: 'public/manifest.json', dest: '.' },
-                { src: 'public/login.html', dest: '.' },
-                { src: 'public/icons', dest: '.' }
-            ]
-        })
-    ]
+    }
 })
